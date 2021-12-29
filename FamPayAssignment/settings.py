d608d5361e7252e0f88429d5003740a1972b4237
MuskanPaliwal <muskan10112002@gmail.com>
2021-12-29 10:32:54 +0530

    backend: started django app

--- FamPayAssignment/settings.py
+++ FamPayAssignment/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest-framework'
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
