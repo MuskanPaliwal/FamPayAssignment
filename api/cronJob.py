051b7d756308abd15472893cc1dc8ca6806ec1d2
MuskanPaliwal <muskan10112002@gmail.com>
2021-12-29 11:42:51 +0530

    backend: added support for multiple api keys

--- api/cronJob.py
+++ api/cronJob.py
@@ -4,6 +4,7 @@ from django_cron import CronJobBase, Schedule
 
 #Google API
 from apiclient.discovery import build
+import apiclient
 
 from .models import *
 from FamPayAssignment import settings
@@ -14,30 +15,47 @@ class CallYoutubeApi(CronJobBase):
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'api.call_youtube_api'    # a unique code
-    time_now = datetime.now()
-    last_request_time = time_now - timedelta(minutes=5)
 
     def do(self):
-        apiKey = settings.GOOGLE_API_KEY
-        youtube = build("youtube", "v3", developerKey= apiKey)
+        apiKeys = settings.GOOGLE_API_KEYS
+        time_now = datetime.now()
+        last_request_time = time_now - timedelta(minutes=5)
 
-        req = youtube.search().list(q = "cricket", part = "snippet", order = "date", maxResults=50, publishedAfter=(self.last_request_time.replace(microsecond=0).isoformat()+'Z'))
-        res = req.execute()
+        valid = False
 
-        for item in res['items']:
-            video_id = item['id']['videoId']
-            publishedDateTime = item['snippet']['publishedAt']
-            title = item['snippet']['title']
-            description = item['snippet']['description']
-            thumbnailsUrls = item['snippet']['thumbnails']['default']['url' ]
-            channel_id = item['snippet']['channelId']
-            channel_title = item['snippet']['channelTitle']
-            Videos.objects.create(
-                video_id = video_id,
-                title = title,
-                description = description,
-                channel_id =channel_id,
-                channel_title = channel_title,
-                publishedDateTime = publishedDateTime,
-                thumbnailsUrls = thumbnailsUrls,
-            )
+        for apiKey in apiKeys:
+            try:
+                youtube = build("youtube", "v3", developerKey=apiKey)
+                req = youtube.search().list(q="cricket", part="snippet", order="date", maxResults=50, publishedAfter=(last_request_time.replace(microsecond=0).isoformat()+'Z'))
+                res = req.execute()
+                valid = True
+            except apiclient.errors.HttpError as err:
+                code = err.resp.status
+                if not(code == 400 or code == 403):
+                    break
+
+            if valid:
+                break
+
+        if valid:
+
+            ### To create an object in DB if there is a valid API Key
+
+            for item in res['items']:
+                video_id = item['id']['videoId']
+                publishedDateTime = item['snippet']['publishedAt']
+                title = item['snippet']['title']
+                description = item['snippet']['description']
+                thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
+                channel_id = item['snippet']['channelId']
+                channel_title = item['snippet']['channelTitle']
+                print(title)
+                Videos.objects.create(
+                    video_id=video_id,
+                    title=title,
+                    description=description,
+                    channel_id=channel_id,
+                    channel_title=channel_title,
+                    publishedDateTime=publishedDateTime,
+                    thumbnailsUrls=thumbnailsUrls,
+                )
--- api/views.py
+++ api/views.py
@@ -7,15 +7,18 @@ from .serializers import *
 #Rest FrameWork
 from rest_framework import generics
 from rest_framework.pagination import CursorPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ResultsPagination(CursorPagination):
-    page_size = 10
+    page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
 
 class YoutubeItems(generics.ListAPIView):
-    search_fields = ['title']
-    filter_backends = (filters.SearchFilter)
-    queryset = Videos.objects.all().order_by('-publishedDateTime')
+    search_fields = ['title', 'description']
+    filter_backends = (filters.SearchFilter,DjangoFilterBackend,filters.OrderingFilter)
+    filterset_fields = ['channel_id','channel_title']
+    ordering = ('-publishedDateTime')
+    queryset = Videos.objects.all()
     serializer_class = VideosSerializer
     pagination_class = ResultsPagination
